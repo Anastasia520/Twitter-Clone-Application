0995af227858a0bb78fb97b38c4916161a15cddf
Stacy <pnastya5@mail.ru>
2023-08-24 13:33:12 +0500

    added BundleAnalyzerPlugin

--- config/build/BuildPlugins.ts
+++ config/build/BuildPlugins.ts
@@ -2,6 +2,8 @@ import path from "path";
 import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+
 import { BuildOptions, BuildPaths } from "./types/config";
 
 export function buildPlugins(
@@ -20,5 +22,6 @@ export function buildPlugins(
             __IS_DEV__: JSON.stringify(isDev),
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({ openAnalyzer: false }),
     ];
 }
