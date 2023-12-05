45765dd2d60be330951d4c22a2e4b0e7a26e14ab
Stacy <pnastya5@mail.ru>
2023-12-05 23:08:45 +0500

    changed sidebar, added new btn variants

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,26 +4,40 @@ import cls from "./Button.module.scss";
 
 export enum ThemeButton {
   CLEAR = "clear",
-  OUTLINE ="outline"
+  OUTLINE = "outline",
+  BACKGROUND = "background",
+  BACKGROUND_INVERTED = "backgroundInverted",
+}
+
+export enum SizeButton {
+  M = "size_m",
+  L = "size_l",
+  XL = "size_xl",
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ThemeButton;
+  square?: boolean;
+  size?: SizeButton;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-    const {
-        className, children, theme, ...otherProps
-    } = props;
+  const { className, square, children, theme, size, ...otherProps } = props;
 
-    return (
-        <button
-            type="button"
-            className={classNames(cls.Button, {}, [className, cls[theme]])}
-            {...otherProps}
-        >
-            {children}
-        </button>
-    );
+  const mods: Record<string, boolean> = {
+    [cls[theme]]: true,
+    [cls.square]: square,
+    [cls[size]]: true,
+  };
+
+  return (
+    <button
+      type="button"
+      className={classNames(cls.Button, mods, [className])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 };
